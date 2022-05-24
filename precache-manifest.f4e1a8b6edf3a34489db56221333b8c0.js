97b9e820d97b705ba9a8b6cc0c6cfb9fb1f8bfb0
Hengly Aun <aunhengly@gmail.com>
2022-05-24 14:19:52 -0700

    Updates

---
+++ precache-manifest.f4e1a8b6edf3a34489db56221333b8c0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7809b1581bea8bc5bf2c",
+    "url": "/robots_4-redux-pwa/static/css/main.38d4e0b7.chunk.css"
+  },
+  {
+    "revision": "7809b1581bea8bc5bf2c",
+    "url": "/robots_4-redux-pwa/static/js/main.67f520a6.chunk.js"
+  },
+  {
+    "revision": "c8e77024c44c6ab1ef25",
+    "url": "/robots_4-redux-pwa/static/js/runtime~main.946ec7ae.js"
+  },
+  {
+    "revision": "0a0c295b89a590218465",
+    "url": "/robots_4-redux-pwa/static/css/2.3108cd18.chunk.css"
+  },
+  {
+    "revision": "0a0c295b89a590218465",
+    "url": "/robots_4-redux-pwa/static/js/2.fea135e9.chunk.js"
+  },
+  {
+    "revision": "f019a8d8b6b121e06049",
+    "url": "/robots_4-redux-pwa/static/js/3.ea86ce1b.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robots_4-redux-pwa/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "fa43da960cf3b5304c97f368450f835c",
+    "url": "/robots_4-redux-pwa/index.html"
+  }
+];
